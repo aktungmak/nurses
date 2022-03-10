e42024f87dbaa9e8a3f78052c6138a8a7f1afa47
Joshua Green <joshua.green@klarna.com>
2022-03-10 10:32:13 +0100

    add GNU prolog version

--- constraints.pl
+++ constraints.pl
@@ -3,39 +3,22 @@ employee_max_shifts(pernilla, 12).
 employee_max_shifts(gun, 12).
 employee_max_shifts(annmarie, 12).
 
-employee_skills(joy, [dogs, cats]).
-employee_skills(pernilla, [dogs, cats, horses]).
-employee_skills(gun, [birds]).
-employee_skills(annmarie, [medicines]).
+employee_skills(rebecca, [gastro, mmt]).
+employee_skills(josefin, [imed, akut]).
+employee_skills(emelie, [ultraljud, hud]).
 
-task_skills(triage, [dogs, cats]).
+task_skills(outpatient_ward, []).
 task_skills(neutering, [dogs, cats]).
 task_skills(horseshoe_maintenance, [horses]).
 task_skills(pharmacy, [medicines]).
 
-employee_unavailable(joy, shift(friday, 1)).
-employee_unavailable(joy, shift(friday, 2)).
-employee_unavailable(joy, shift(saturday, 1)).
-employee_unavailable(joy, shift(saturday, 2)).
-employee_unavailable(pernilla, shift(sunday, _)).
+employee_unavailable(kava, shift(thursday_3, _)).
 
-task(neutering, shift(saturday, 1)).
-task(neutering, shift(monday, 2)).
-task(triage, shift(monday, 1)).
-task(triage, shift(monday, 2)).
-task(triage, shift(tuesday, 1)).
-task(triage, shift(tuesday, 2)).
-task(triage, shift(wednesday, 1)).
-task(triage, shift(wednesday, 2)).
-task(pharmacy, shift(monday, 1)).
-task(pharmacy, shift(monday, 2)).
-task(pharmacy, shift(tuesday, 1)).
-task(pharmacy, shift(tuesday, 2)).
-task(pharmacy, shift(wednesday, 1)).
-task(pharmacy, shift(wednesday, 2)).
-task(horseshoe_maintenance, shift(friday, 1)).
+task(gastro, shift(tuesday, daytime)).
+task(gastro, shift(thursday, daytime)).
+task(hud, shift(monday, daytime)).
 
-employee_assigned(joy, task(triage, shift(monday, 1))).
-employee_assigned(pernilla, task(triage, shift(monday, 2))).
-employee_assigned(joy, task(triage, shift(tuesday, 1))).
-employee_assigned(joy, task(triage, shift(tuesday, 2))).
+
+employee_assigned(kava, task(hud, shift(monday, daytime))).
+employee_assigned(kava, task(hud, shift(wednesday, evening))).
+employee_assigned(kava, task(hud, shift(friday_3, daytime))).
